a9205986d27d8c2a15078491b34017c15ebb3917
Barney <B.Wilton1993@gmail.com>
2023-05-02 14:00:39 +0100

    Finished ex 1 and chal 1

---
+++ phase_two/hello_web_project/app.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/hello' do
+    name = params[:name] # The value is 'David'
+  
+    # Do something with `name`...
+  
+    return "Hello #{name}."
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Hello #{name}, you sent this message: #{message}"
+  end
+end
